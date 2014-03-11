297b89e70fbc57db36ae5e1ccb34edfc27f066ef
Jonathan Wang <jonathanwang017@berkeley.edu>
2014-03-10 21:56:56 -0700

    make webpage to log project
    for my own practice/entertainment

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#header_subtitle").mouseover(function() {
+        $("#header_subtitle").text("Berkeley - Team LiveMau5");
+    });
+    $("#header_subtitle").mouseleave(function() {
+        $("#header_subtitle").text("A record of our robot building experience");
+    });
+    $("#parts_header").click(function() {
+        $("#parts_sect").toggle();
+    });
+    $("#building_header").click(function() {
+        $("#building_sect").toggle();
+    });
+    $("#wiring_header").click(function() {
+        $("#wiring_sect").toggle();
+    });
+    $("#operations_header").click(function() {
+        $("#operations_sect").toggle();
+    });
+    $("#algorithms_header").click(function() {
+        $("#algorithms_sect").toggle();
+    });
+});
