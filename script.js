76a728784f7e5605f658981a06f8dd513b340c42
Jonathan Wang <jonathanwang017@berkeley.edu>
2014-04-17 10:20:23 -0700

    update site

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ $(document).ready(function() {
     $("#wiring_header").click(function() {
         $("#wiring_sect").toggle();
     });
+    $("#challenges_header").click(function() {
+        $("#challenges_sect").toggle();
+    });
     $("#operations_header").click(function() {
         $("#operations_sect").toggle();
     });
