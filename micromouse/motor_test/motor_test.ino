8e5c8d20d2f329d48417ab4e0c89eb048cb36c12
Jonathan Wang <jonathanwang017@berkeley.edu>
2014-04-18 16:48:31 -0700

    derive motor and sensor stuff
    the move, turn functions and wall checking are in here somewhere.

--- micromouse/motor_test/motor_test.ino
+++ micromouse/motor_test/motor_test.ino
@@ -4,17 +4,25 @@ const int motor3 = 21;
 const int leftEnable = 2;
 const int motor1 = 3;
 const int motor2 = 4;
-const int leftEncoderA = 9;
-const int leftEncoderB = 8;
-const int rightEncoderA = 22;
-const int rightEncoderB = 23;
+const int leftEncoderA = 8;
+const int leftEncoderB = 9;
+const int rightEncoderA = 23;
+const int rightEncoderB = 22;
 
 const int led = 13;
 
-int leftEncoderPos = 0;
-int rightEncoderPos = 0;
+volatile int leftEncoderPos;
+volatile int rightEncoderPos;
+
+volatile int leftMotorState;
+volatile int rightMotorState;
 
 void setup() {
+  leftEncoderPos = 0;
+  rightEncoderPos = 0;
+  leftMotorState = HIGH;
+  rightMotorState = HIGH;
+  
   pinMode(rightEnable, OUTPUT);
   pinMode(motor3, OUTPUT);
   pinMode(motor4, OUTPUT);
@@ -23,6 +31,8 @@ void setup() {
   pinMode(motor2, OUTPUT);
   digitalWrite(rightEnable, LOW);
   digitalWrite(leftEnable, LOW);
+  Serial.begin(9600);
+  
   pinMode(led, OUTPUT);
   digitalWrite(led, HIGH);
   
@@ -30,43 +40,83 @@ void setup() {
   pinMode(leftEncoderB, INPUT);
   pinMode(rightEncoderA, INPUT);
   pinMode(rightEncoderB, INPUT);
+  digitalWrite(leftEncoderA, HIGH);
+  digitalWrite(leftEncoderB, HIGH);
+  digitalWrite(rightEncoderA, HIGH);
+  digitalWrite(rightEncoderB, HIGH);
   
-  attachInterrupt(leftEncoderA, doLeftEncoder, CHANGE);
-  attachInterrupt(rightEncoderA, doRightEncoder, CHANGE);
+  attachInterrupt(rightEncoderA, checkEncoder, CHANGE);
   
-  digitalWrite(rightEnable, HIGH);
-  digitalWrite(leftEnable, HIGH);
-  digitalWrite(motor1, LOW);
-  digitalWrite(motor2, HIGH);
-  digitalWrite(motor3, LOW);
-  digitalWrite(motor4, HIGH);
+  delay(3000);
+  
+  moveForward();
+  delay(1000);
+  turnRight();
+  delay(1000);
+  moveForward();
+  
 }
 
 void loop() {
-
+//  digitalWrite(rightEnable, rightMotorState);
+//  digitalWrite(leftEnable, leftMotorState);
 }
 
-void doLeftEncoder() {
-  if (digitalRead(leftEncoderA) == digitalRead(leftEncoderB)) {
-    leftEncoderPos++;
-  } else {
-    leftEncoderPos--;
-  }
-  
-  if (leftEncoderPos == 12) {
-    //digitalWrite(leftEnable, LOW);
-  }
+void moveForward() {
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, LOW);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, LOW);
+  digitalWrite(rightEnable, HIGH);
+  digitalWrite(leftEnable, HIGH);
+  delay(215);
+  digitalWrite(rightEnable, LOW);
+  digitalWrite(leftEnable, LOW);
 }
 
-void doRightEncoder() {
-  if (digitalRead(rightEncoderA) == digitalRead(rightEncoderB)) {
-    rightEncoderPos++;
-  } else {
-    rightEncoderPos--;
-  }
-  
-  if (rightEncoderPos == 12) {
-    //digitalWrite(rightEnable, LOW);
-  }
+void turnRight() {
+  digitalWrite(motor1, HIGH);
+  digitalWrite(motor2, LOW);
+  digitalWrite(motor3, LOW);
+  digitalWrite(motor4, HIGH);
+  digitalWrite(rightEnable, HIGH);
+  digitalWrite(leftEnable, HIGH);
+  delay(95);
+  digitalWrite(rightEnable, LOW);
+  digitalWrite(leftEnable, LOW);
 }
 
+void turnLeft() {
+  digitalWrite(motor1, LOW);
+  digitalWrite(motor2, HIGH);
+  digitalWrite(motor3, HIGH);
+  digitalWrite(motor4, LOW);
+  digitalWrite(rightEnable, HIGH);
+  digitalWrite(leftEnable, HIGH);
+  delay(95);
+  digitalWrite(rightEnable, LOW);
+  digitalWrite(leftEnable, LOW);
+}
+
+void checkEncoder() {
+  if (digitalRead(rightEncoderA) == HIGH) {   
+    if (digitalRead(rightEncoderB) == LOW) {  
+      rightEncoderPos--;        
+    } 
+    else {
+      rightEncoderPos++;
+    }
+  }
+  else { 
+    if (digitalRead(rightEncoderB) == LOW) {   
+      rightEncoderPos++;         
+    } 
+    else {
+      rightEncoderPos--;         
+    }
+  }
+  if (rightEncoderPos == -20) {
+    leftMotorState = LOW;
+    rightMotorState = LOW;
+  }
+}
